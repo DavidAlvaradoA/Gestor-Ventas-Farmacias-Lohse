0054085074c6ef4405f3e080e8cc80d1f778d922
DavidAlvaradoA <david.alv.alf@gmail.com>
2024-06-24 11:27:20 -0400

    Correción rut

--- views/modulo-usuarios/usuarios/create.php
+++ views/modulo-usuarios/usuarios/create.php
@@ -7,6 +7,8 @@ include ('../../../App/controllers/roles/listado_roles.php');
 
 ?>
 
+<script type="module" src="https://unpkg.com/html-input-rut"></script>
+
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
   <!-- Content Header (Page header) -->
@@ -57,10 +59,61 @@ include ('../../../App/controllers/roles/listado_roles.php');
                                 placeholder="Ingrese apellidos..." required>
                             </div>
                           </div>
+
+                          <script>
+                            function checkRut(rut) {
+                              // Despejar Puntos
+                              var valor = rut.value.replace('.', '');
+                              // Despejar Guión
+                              valor = valor.replace('-', '');
+
+                              // Aislar Cuerpo y Dígito Verificador
+                              cuerpo = valor.slice(0, -1);
+                              dv = valor.slice(-1).toUpperCase();
+
+                              // Formatear RUN
+                              rut.value = cuerpo + '-' + dv
+
+                              // Si no cumple con el mínimo ej. (n.nnn.nnn)
+                              if (cuerpo.length < 7) { rut.setCustomValidity("RUT Incompleto"); return false; }
+
+                              // Calcular Dígito Verificador
+                              suma = 0;
+                              multiplo = 2;
+
+                              // Para cada dígito del Cuerpo
+                              for (i = 1; i <= cuerpo.length; i++) {
+
+                                // Obtener su Producto con el Múltiplo Correspondiente
+                                index = multiplo * valor.charAt(cuerpo.length - i);
+
+                                // Sumar al Contador General
+                                suma = suma + index;
+
+                                // Consolidar Múltiplo dentro del rango [2,7]
+                                if (multiplo < 7) { multiplo = multiplo + 1; } else { multiplo = 2; }
+
+                              }
+
+                              // Calcular Dígito Verificador en base al Módulo 11
+                              dvEsperado = 11 - (suma % 11);
+
+                              // Casos Especiales (0 y K)
+                              dv = (dv == 'K') ? 10 : dv;
+                              dv = (dv == 0) ? 11 : dv;
+
+                              // Validar que el Cuerpo coincide con su Dígito Verificador
+                              if (dvEsperado != dv) { rut.setCustomValidity("RUT Inválido"); return false; }
+
+                              // Si todo sale bien, eliminar errores (decretar que es válido)
+                              rut.setCustomValidity('');
+                            }
+                          </script>
+                          
                           <div class="col-md-4">
                             <div class="form-group">
                               <label for="">Rut:</label>
-                              <input type="text" name="rut" class="form-control" placeholder="Ingrese rut..." required>
+                              <input type="text" name="rut" class="form-control" required oninput="checkRut(this)" placeholder="Ingrese rut..." required>
                             </div>
                           </div>
                         </div>
